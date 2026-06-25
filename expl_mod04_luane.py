7df295b3d0c3d5a366fd270098d5ed0f8cc3de9c
Aluna Vocação - Luane Penafort <luanepenafortns@gmail.com>
2026-06-25 16:59:02 -0300

    Add files via upload

---
+++ expl_mod04_luane.py
@@ -0,0 +1,12 @@
+frutas = ["uva", "maça", "pitaya", "pera", "banana"]
+print(frutas) #['uva', 'maça', 'pitaya', 'pera', 'banana']
+
+print('Qual a fruta que você deseja adicionar na lista: ?')
+
+nova_fruta = input('A fruta escolhida foi: ')
+frutas.append(nova_fruta)
+print(f'Essas são as nossas frutas: {frutas}')
+
+
+# variavel + atributo devo usar o ponto "frutas.append(nova_fruta)"
+# para adicionar a fruta na lista
